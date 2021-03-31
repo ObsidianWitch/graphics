a5af0204fb0391dc173f8620efbc035e4f2d4dc1
Obsidienne <obsidienne@protonmail.com>
2021-04-01 00:08:36 +0200

    DimetricGrid: refactor

--- Scripts/DimetricGrid.py
+++ Scripts/DimetricGrid.py
@@ -9,33 +9,39 @@ from PIL import Image, ImageDraw
 from pathlib import Path
 
 class Edge:
-    def __init__(self, segw, segh, n):
-        self.segw = segw
-        self.segh = segh
-        self.n = n
+    def __init__(self, segwidth, segheight, segn):
+        self.segwidth = segwidth
+        self.segheight = segheight
+        self.segn = segn
 
     def width(self):
-        return self.n * self.segw
+        return self.segn * self.segwidth
 
     def height(self):
-        return self.n * self.segh
+        return self.segn * self.segheight
 
-    def generator(self):
-        for i in range(self.n):
-            x = self.width() - (i + 1) * self.segw
-            y = self.segh * i
+    def size(self):
+        return (self.width(), self.height())
+
+    def image(self, fgcolor, bgcolor):
+        img = Image.new('RGBA', self.size(), bgcolor)
+        drawtool = ImageDraw.Draw(img)
+
+        for i in range(self.segn):
+            x = self.width() - (i + 1) * self.segwidth
+            y = self.segheight * i
             p1 = (x, y)
-            p2 = (x + self.segw - 1, y + self.segh - 1)
-            yield (p1, p2)
+            p2 = (x + self.segwidth - 1, y + self.segheight - 1)
+            drawtool.rectangle((p1, p2), fgcolor)
+
+        return img
 
     def __str__(self):
-        return f"e{self.segw}x{self.segh}x{self.n}"
+        return f"e{self.segwidth}x{self.segheight}x{self.segn}"
 
 class Tile:
-    def __init__(self, edge, fgcolor = (0, 0, 0), bgcolor = None):
+    def __init__(self, edge):
         self.edge = edge
-        self.fgcolor = fgcolor
-        self.bgcolor = bgcolor
 
     def width(self):
         return 2 * self.edge.width()
@@ -46,22 +52,12 @@ class Tile:
     def size(self):
         return [self.width(), self.height()]
 
-    def image(self):
-        bg = Image.new('RGBA', self.size(), self.bgcolor)
-        fg = Image.new('RGBA', self.size())
-        drawfg = ImageDraw.Draw(fg)
-
-        for box in self.edge.generator():
-            drawfg.rectangle(box, self.fgcolor)
-        fg.alpha_composite(fg.transpose(Image.FLIP_LEFT_RIGHT))
-        fg.alpha_composite(fg.transpose(Image.FLIP_TOP_BOTTOM))
-        bg.alpha_composite(fg)
-        return bg
-
-    def save(self):
-        filename = Path('out') / f"{self}.png"
-        filename.parent.mkdir(exist_ok=True)
-        self.image().save(filename)
+    def image(self, fgcolor, bgcolor):
+        img = Image.new('RGBA', self.size())
+        img.paste(self.edge.image(fgcolor, bgcolor), (0, 0))
+        img.alpha_composite(img.transpose(Image.FLIP_LEFT_RIGHT))
+        img.alpha_composite(img.transpose(Image.FLIP_TOP_BOTTOM))
+        return img
 
     def __str__(self):
         return f"t{self.width()}x{self.height()}_{self.edge}"
@@ -80,24 +76,26 @@ class Grid:
     def size(self):
         return [self.width(), self.height()]
 
-    def image(self):
-        tileimg = self.tile.image()
+    def image(self, fgcolor, bgcolor):
+        tileimg = self.tile.image(fgcolor, bgcolor)
         gridimg = Image.new('RGBA', self.size())
         for x in range(0, self.width(), self.tile.width()):
             for y in range(0, self.height(), self.tile.height()):
                 gridimg.paste(tileimg, (x, y))
         return gridimg
 
-    def save(self):
-        filename = Path('out') / f"{self}.png"
-        filename.parent.mkdir(exist_ok=True)
-        self.image().save(filename)
-
     def __str__(self):
         return f"g{self.width()}x{self.height()}_{self.tile}"
 
+def save(element):
+    filename = Path('out') / f"{element}.png"
+    filename.parent.mkdir(exist_ok=True)
+    element.image(fgcolor="#4B5263", bgcolor="#ABB2BF") \
+           .save(filename)
+
 if __name__ == "__main__":
-    Grid(tile = Tile(edge = Edge(segw=4, segh=2, n=8),
-                     fgcolor = "#4B5263",
-                     bgcolor = "#ABB2BF"),
-         n = [10, 10]).save()
+    grid = Grid(
+        tile = Tile(Edge(segwidth=4, segheight=2, segn=8)),
+        n = (10, 10)
+    )
+    save(grid)
