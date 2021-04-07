aedcebc2853564c02371ee00fb00542fa1ad2ee7
Obsidienne <obsidienne@protonmail.com>
2021-04-07 11:00:34 +0200

    Blender:AnimationTutorial: cubes collection

--- Blender/AnimationTutorial.py
+++ Blender/AnimationTutorial.py
@@ -9,6 +9,12 @@ for data_type in (bpy.data.actions, bpy.data.cameras, bpy.data.lights,
                   bpy.data.meshes, bpy.data.objects, bpy.data.collections):
     for item in data_type: data_type.remove(item)
 
+# cubes collection
+cubes_collection = bpy.data.collections.new('cubes')
+bpy.context.scene.collection.children.link(cubes_collection)
+view_layer = bpy.context.view_layer
+view_layer.active_layer_collection = view_layer.layer_collection.children[cubes_collection.name]
+
 # 10*10 cubes grid
 bpy.ops.mesh.primitive_cube_add(size=2)
 for _ in range(9):
