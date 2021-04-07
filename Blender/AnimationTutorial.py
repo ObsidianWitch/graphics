e6b20cabc3b94b1342587e3812627a2216414c23
Obsidienne <obsidienne@protonmail.com>
2021-04-07 11:22:21 +0200

    Blender:AnimationTutorial: animation

--- Blender/AnimationTutorial.py
+++ Blender/AnimationTutorial.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env -S blender --factory-startup --python
 
 # ref: [CG Cookie - Animating With Python](https://www.youtube.com/watch?v=QnvN1dieIAU)
+# blender version: 2.92.0
 
 import bpy
 
@@ -16,7 +17,7 @@ view_layer = bpy.context.view_layer
 view_layer.active_layer_collection = view_layer.layer_collection.children[cubes_collection.name]
 
 # 10*10 cubes grid
-bpy.ops.mesh.primitive_cube_add(size=2)
+bpy.ops.mesh.primitive_cube_add()
 for _ in range(9):
     bpy.ops.object.duplicate(linked=True)
     bpy.ops.transform.translate(value=(0, 2, 0))
@@ -24,3 +25,15 @@ bpy.ops.object.select_all(action='SELECT')
 for _ in range(9):
     bpy.ops.object.duplicate(linked=True)
     bpy.ops.transform.translate(value=(2, 0, 0))
+
+# animation
+bpy.data.scenes['Scene'].frame_end = 80 + len(cubes_collection.objects)
+for i, obj in enumerate(cubes_collection.objects):
+    obj.scale = (0, 0, 0)
+    obj.keyframe_insert(data_path='scale', frame=1 + i)
+    obj.scale = (1, 1, 5)
+    obj.keyframe_insert(data_path='scale', frame=50 + i)
+    obj.scale = (1, 1, 0.5)
+    obj.keyframe_insert(data_path='scale', frame=70 + i)
+    obj.scale = (1, 1, 1)
+    obj.keyframe_insert(data_path='scale', frame=80 + i)
