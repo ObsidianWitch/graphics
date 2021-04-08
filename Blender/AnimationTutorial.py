4d2a5800b1fc6f655ecf689e51addd5e99000894
Obsidienne <obsidienne@protonmail.com>
2021-04-08 09:02:01 +0200

    Blender:AnimationTutorial: delete collection & reduce bpy.ops usage

--- Blender/AnimationTutorial.py
+++ Blender/AnimationTutorial.py
@@ -1,34 +1,30 @@
 #!/usr/bin/env -S blender --factory-startup --python
 
 # ref: [CG Cookie - Animating With Python](https://www.youtube.com/watch?v=QnvN1dieIAU)
+# ref: [BPY Documentation](https://docs.blender.org/api/current/)
+# ref: [Python Performance with Blender operators](https://blender.stackexchange.com/a/7360)
 # blender version: 2.92.0
 
-import bpy
+import bpy, itertools
 
 bpy.context.preferences.view.show_splash = False
 for data_type in (bpy.data.actions, bpy.data.cameras, bpy.data.lights,
                   bpy.data.meshes, bpy.data.objects, bpy.data.collections):
     for item in data_type: data_type.remove(item)
 
-# cubes collection
-cubes_collection = bpy.data.collections.new('cubes')
-bpy.context.scene.collection.children.link(cubes_collection)
-view_layer = bpy.context.view_layer
-view_layer.active_layer_collection = view_layer.layer_collection.children[cubes_collection.name]
-
 # 10*10 cubes grid
 bpy.ops.mesh.primitive_cube_add()
-for _ in range(9):
-    bpy.ops.object.duplicate(linked=True)
-    bpy.ops.transform.translate(value=(0, 2, 0))
-bpy.ops.object.select_all(action='SELECT')
-for _ in range(9):
-    bpy.ops.object.duplicate(linked=True)
-    bpy.ops.transform.translate(value=(2, 0, 0))
+cubetpl = bpy.context.active_object
+for x, y in itertools.product(range(10), repeat=2):
+    if x == 0 and y == 0: continue
+    c = cubetpl.copy()
+    c.location[0] = x * 2
+    c.location[1] = y * 2
+    bpy.data.scenes['Scene'].collection.objects.link(c)
 
 # animation
-bpy.data.scenes['Scene'].frame_end = 80 + len(cubes_collection.objects)
-for i, obj in enumerate(cubes_collection.objects):
+bpy.data.scenes['Scene'].frame_end = 80 + len(bpy.data.objects)
+for i, obj in enumerate(bpy.data.objects):
     obj.scale = (0, 0, 0)
     obj.keyframe_insert(data_path='scale', frame=1 + i)
     obj.scale = (1, 1, 5)
