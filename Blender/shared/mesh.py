4c5f770dee374dbb9118913a215606b688074680
Obsidienne <obsidienne@protonmail.com>
2021-06-22 11:06:41 +0200

    blender:shared: bmesh2obj()

--- Blender/shared/mesh.py
+++ Blender/shared/mesh.py
@@ -1,10 +1,13 @@
 import bpy, bmesh
 
+def bmesh2obj(bm, name):
+    mesh = bpy.data.meshes.new(name)
+    bm.to_mesh(mesh)
+    return bpy.data.objects.new(name, mesh)
+
 def new_cube(*args, **kwargs):
     bm = bmesh.new()
     bmesh.ops.create_cube(bm, *args, **kwargs)
-    mesh = bpy.data.meshes.new('Cube')
-    bm.to_mesh(mesh)
+    obj = bmesh2obj(bm, name='Cube')
     bm.free()
-    obj = bpy.data.objects.new('Cube', mesh)
     return obj
