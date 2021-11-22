6a035514c7b0900ad2df4ab2891a9efe6969ea2c
Obsidienne <obsidienne@protonmail.com>
2021-11-22 03:48:38 +0100

    bpy:03: reference

--- Blender/03NewsCaster.py
+++ Blender/03NewsCaster.py
@@ -17,6 +17,17 @@ class Character:
     def list(cls):
         return []
 
+def setup_reference():
+    bpy.ops.wm.append(filepath="03Reference.blend/Collection/References",
+                      directory="03Reference.blend/Collection",
+                      filename="References")
+    references = bpy.data.collections['References'].objects
+    for obj in references:
+        obj.hide_viewport = True
+        obj.show_in_front = True
+        obj.display_type = 'WIRE'
+    references['leg'].hide_viewport = False
+
 def setup_scene():
     scene = bpy.data.scenes[0]
     for part in Character.list():
@@ -24,4 +35,5 @@ def setup_scene():
 
 if __name__ == '__main__':
     shared.delete_data()
+    setup_reference()
     setup_scene()
