320359823ed9a7e9b4308949a0719fd79325a8cb
Obsidienne <obsidienne@protonmail.com>
2021-12-29 23:23:36 +0100

    bpy:04: add_limb_two_loops()

--- BPY/04LimbTopology.py
+++ BPY/04LimbTopology.py
@@ -19,22 +19,14 @@ importlib.reload(shared)
 def add_limb_naive() -> bpy.types.Object:
     # bmesh
     bm = bmesh.new()
-    bmesh.ops.create_circle(bm, segments=16, radius=0.1)
-    c1 = { 'verts': bm.verts[:], 'edges': bm.edges[:], 'faces': bm.faces[:] }
-    c2 = bmesh.ops.extrude_edge_only(bm, edges=c1['edges'])
-    c2 = shared.bm_geom_split(c2['geom'])
-    bmesh.ops.translate(bm, verts=c2['verts'], vec=(0, 0, 0.2))
-    c3 = bmesh.ops.extrude_edge_only(bm, edges=c2['edges'])
-    c3 = shared.bm_geom_split(c3['geom'])
-    bmesh.ops.translate(bm, verts=c3['verts'], vec=(0, 0, 0.2))
-    bmesh.ops.translate(bm, verts=bm.verts, vec=(0, 0, -c2['verts'][0].co.z))
+    bm_cylinder_zcuts(bm, segments=16, radius=0.1, zcuts=(0.2, 0.2))
 
     # mesh & object
     mesh = D.meshes.new('limb_naive')
     object = D.objects.new(mesh.name, mesh)
     C.scene.collection.objects.link(object)
 
-    # Vertex groups
+    # vertex groups
     # ref: https://docs.blender.org/api/current/bmesh.html#customdata-access
     deform_layer = bm.verts.layers.deform.new()
     b1_vg = object.vertex_groups.new(name='limb.001')
@@ -46,9 +38,47 @@ def add_limb_naive() -> bpy.types.Object:
     bm.to_mesh(mesh)
     bm.free()
 
-    # Armature
-    # ref: https://www.youtube.com/watch?v=cZ3o5tjO51s
-    # ref: https://blender.stackexchange.com/a/51697
+    # armature
+    return add_limb_armature(object, b1_vg.name, b2_vg.name)
+
+def add_limb_two_loops() -> bpy.types.Object:
+    # bmesh
+    bm = bmesh.new()
+    bm_cylinder_zcuts(bm, segments=16, radius=0.1, zcuts=(0.19, 0.01, 0.2))
+
+    # mesh & object
+    mesh = D.meshes.new('limb_two_loops')
+    object = D.objects.new(mesh.name, mesh)
+    C.scene.collection.objects.link(object)
+
+    # vertex groups
+    # ref: https://docs.blender.org/api/current/bmesh.html#customdata-access
+    deform_layer = bm.verts.layers.deform.new()
+    b1_vg = object.vertex_groups.new(name='limb.001')
+    for vert in bm.verts[0:32]:
+        vert[deform_layer][b1_vg.index] = 1.0
+    b2_vg = object.vertex_groups.new(name='limb.002')
+    for vert in bm.verts[32:]:
+        vert[deform_layer][b2_vg.index] = 1.0
+    bm.to_mesh(mesh)
+    bm.free()
+
+    # armature
+    return add_limb_armature(object, b1_vg.name, b2_vg.name)
+
+def bm_cylinder_zcuts(bm, zcuts, **kwargs):
+    bmesh.ops.create_circle(bm, **kwargs)
+    c_old = { 'verts': bm.verts[:], 'edges': bm.edges[:], 'faces': bm.faces[:] }
+    for z in zcuts:
+        c_new = bmesh.ops.extrude_edge_only(bm, edges=c_old['edges'])
+        c_new = shared.bm_geom_split(c_new['geom'])
+        bmesh.ops.translate(bm, verts=c_new['verts'], vec=(0, 0, z))
+        c_old = c_new
+
+# ref: https://www.youtube.com/watch?v=cZ3o5tjO51s
+# ref: https://blender.stackexchange.com/a/51697
+def add_limb_armature(object, b1_name, b2_name):
+    # armature
     armature = D.armatures.new('armature')
     armature = D.objects.new(armature.name, armature)
     armature.show_in_front = True
@@ -57,27 +87,27 @@ def add_limb_naive() -> bpy.types.Object:
     C.view_layer.objects.active = armature
     bpy.ops.object.mode_set(mode='EDIT')
     bones = armature.data.edit_bones
-    b1 = bones.new(b1_vg.name)
-    b1.head = (0.0, 0.0, -0.3)
-    b1.tail = (0.0, 0.0, 0.0)
-    b2 = bones.new(b2_vg.name)
+    b1 = bones.new(b1_name)
+    b1.head = (0.0, 0.0, 0.0)
+    b1.tail = (0.0, 0.0, 0.2)
+    b2 = bones.new(b2_name)
     b2.head = (0.0, 0.0, 0.0)
-    b2.tail = (0.0, 0.0, 0.3)
+    b2.tail = (0.0, 0.0, 0.4)
     b2.parent = b1
     b2.use_connect = True
     bpy.ops.object.mode_set(mode='OBJECT')
 
-    # Set armature as parent to object
+    # set armature as parent to object
     object.parent = armature
     modifier = object.modifiers.new(name='armature', type='ARMATURE')
     modifier.object = armature
 
-    # Poses & keyframes
+    # set poses & keyframes
     pose_bones = armature.pose.bones
-    pose_bones[b2_vg.name].keyframe_insert(data_path='rotation_quaternion', frame=1)
-    pose_bones[b2_vg.name].keyframe_insert(data_path='rotation_quaternion', frame=100)
-    pose_bones[b2_vg.name].rotation_quaternion = Quaternion((0, 0, 1), radians(-90))
-    pose_bones[b2_vg.name].keyframe_insert(data_path='rotation_quaternion', frame=50)
+    pose_bones[b2_name].keyframe_insert(data_path='rotation_quaternion', frame=1)
+    pose_bones[b2_name].keyframe_insert(data_path='rotation_quaternion', frame=100)
+    pose_bones[b2_name].rotation_quaternion = Quaternion((0, 0, 1), radians(-90))
+    pose_bones[b2_name].keyframe_insert(data_path='rotation_quaternion', frame=50)
 
     return armature
 
@@ -85,4 +115,6 @@ if __name__ == '__main__':
     shared.delete_data()
     C.scene.frame_current = 1
     C.scene.frame_end = 100
-    add_limb_naive()
+    limb1 = add_limb_naive()
+    limb2 = add_limb_two_loops()
+    limb2.location.x += 0.4
